63fa8e88d7767366ec24110e04b879db2035d7ec
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-06-01 16:34:41 +0100

    Skeleton Animation Fade in

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./styles/card.css";
+import clsx from "clsx";
 
 type Props = {
   children: ReactNode;
@@ -11,7 +12,14 @@ const Card = ({ children, title, childrenClassName }: Props) => {
   return (
     <div className="p-4 rounded-2xl bg-linear-to-br from-card to-card/60 flex flex-col gap-4">
       <h2 className="text-2xl font-bold">{title}</h2>
-      <div className={childrenClassName}>{children}</div>
+      <div
+        className={clsx(
+          childrenClassName,
+          "animate-[fade-in_1s_ease-out_forwards]",
+        )}
+      >
+        {children}
+      </div>
     </div>
   );
 };
